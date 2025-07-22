92c58dfaf46fcd2a5ca013eaacd94e1e6c9ab2d0
Yashwant125 <97943305+Yashwant125@users.noreply.github.com>
2025-07-22 19:12:56 +0530

    Update ResetPassword.js

--- frontend/src/Features/Login/ResetPassword.js
+++ frontend/src/Features/Login/ResetPassword.js
@@ -23,7 +23,7 @@ const ResetPassword = () => {
 
     try {
       setLoading(true);
-      const res = await axios.post('http://localhost:5000/api/auth/reset-password', {
+      const res = await axios.post('https://getfit-v9g1.onrender.com/api/auth/reset-password', {
         phone,
         newPassword,
       });
