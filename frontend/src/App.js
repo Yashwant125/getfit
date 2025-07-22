d3fc0163ff4c347ca4ce631f63f9d9370261b521
Yashwant125 <97943305+Yashwant125@users.noreply.github.com>
2025-07-22 19:40:27 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -93,7 +93,7 @@ const App = () => {
 
   const fetchPlans = async () => {
     try {
-      const res = await axios.get("http://localhost:5000/api/plans");
+      const res = await axios.get("https://getfit-v9g1.onrender.com/api/plans");
       setPlans(res.data);
     } catch (err) {
       console.error("❌ Error fetching plans:", err);
@@ -102,7 +102,7 @@ const App = () => {
 
   const fetchMembers = async () => {
     try {
-      const res = await axios.get("http://localhost:5000/api/members");
+      const res = await axios.get("https://getfit-v9g1.onrender.com/api/members");
       setMembers(res.data);
     } catch (err) {
       console.error("❌ Error fetching members:", err);
